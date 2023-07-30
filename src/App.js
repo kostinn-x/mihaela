5600797b2c4476273900ba6a4204481fe95f5ef8
kostinn <kostinnarthur@gmail.com>
2023-07-30 13:23:27 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Mihaela is the best web designer ever; people know her as Talmazian Gorgonzola.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
         </a>
       </header>
     </div>
